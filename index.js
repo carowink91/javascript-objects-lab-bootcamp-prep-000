0e80b39caef4b599e945f9e7ad469bacbc72bcf1
angelica winkler <caw400@nyu.edu>
2018-04-20 16:59:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ function deleteFromObjectByKey (object, key) {
   var newObject = Object.assign({}, recipes);
   delete newObject.key;
   return newObject;
-  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
 }
