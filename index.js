957e8bd8ef86dbd0919729480d67313e9b100eca
angelica winkler <caw400@nyu.edu>
2018-04-20 17:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromObjectByKey (object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete recipes.key;
-  return recipes;
+  delete object.key;
+  return object;
 }
